37cb5cb59dbb026a65b9dddcc677612f26c4c5d4
patkamon <patkamon.a@ku.th>
2022-05-28 13:01:13 +0700

    receipt and prescription nft

--- contracts/nft/Receipt.sol
+++ contracts/nft/Receipt.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "./Prescription.sol";
 
 contract Prescription is ERC721URIStorage {
     using Counters for Counters.Counter;
@@ -10,23 +11,29 @@ contract Prescription is ERC721URIStorage {
 
     address public owner;
 
-    constructor() ERC721("Prescription", "PST") {
+    constructor(address _prescription) ERC721("Receipt", "RCP") {
         // only owner will be able to do some task.
         owner = msg.sender;
+        prescription = Prescription(_prescription);
     }
 
-    function create(string memory tokenURI, address patient)
-        public
-        returns (uint256)
-    {
-        // only doctor (owner) that create prescription
+    function create(
+        uint256,
+        tokenId,
+        string memory tokenURI,
+        address patient
+    ) public returns (uint256) {
+        // only pharmacist (owner) that create receipt
         require(tx.origin == owner, "Not Owner");
         require(patient != address(0), "Patient Address not exist!");
+
+        // take prescription
+        ERC721(prescription).transferFrom(patient, address(this), tokenId);
 
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
 
-        // mint prescription and send to patient
+        // mint receipt and send to patient
         _mint(patient, newItemId);
 
         // set tokenURI for patient
